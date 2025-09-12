c52312791dccc87ab4c498daec66644587681452
Heinz <hgbeckerss@gmail.com>
2025-09-12 18:31:25 -0300

    FIX: nombre base del proyecto para release, prueba

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const repoName = 'release/staging';
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`, 
+  base: '/arco_v2/',
 });
