a50ca5efc29545de8a8b99f97a564c1cafb42a8b
JJ1313 <JJ1313@users.noreply.github.com>
2025-09-12 21:08:03 +0000

    deploy: 17d95f0772b3cdb8263adc18adac524875c4ea45

--- src/components/MainImage.jsx
+++
@@ -1,7 +0,0 @@
-import './MainImage.css'
-const MainImage = () => {
-return <div className="mainImage__container">
-      <img className="mainImage__image" src={"https://picsum.photos/id/448/1160/740"} />
-  </div>
-}
-export default MainImage;
