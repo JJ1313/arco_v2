f699389ba6332dadac6b737b69a62bff547a2dbb
Heinz <hgbeckerss@gmail.com>
2025-09-12 17:59:20 -0300

    ADD: Funcion base para testear top

--- src/components/FabToTop.jsx
+++ src/components/FabToTop.jsx
@@ -1,5 +1,12 @@
 import './FabToTop.css'
 
-const FabToTop = () => {}
+const FabToTop = () => {
+  const handleClick = () => {
+    console.log("Scroll to top");
+  }
+  return <button onClick={ handleClick}>
+    ToTop
+  </button>
+}
 
 export default FabToTop;
