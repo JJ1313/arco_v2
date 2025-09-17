81a50f6715a1cad67b379ced0f161e59cf3c98b0
Heinz <hgbeckerss@gmail.com>
2025-09-17 14:43:27 -0300

    ADD: fab to top

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,34 @@ import Portfolio from './components/Portfolio'
 import Testimonial from './components/Testimonial'
 import Founders from './components/Founders'
 import ContactForm from './components/ContactForm.jsx'
+import FabToTop from './components/FabToTop.jsx'
 
 const App = () => {
   return (
     <div className="container">
-      <Hero/>
+      <Hero />
       <MainImage />
-      <CharacteristicItem  
-        imgUrl={"https://picsum.photos/id/508/580/580"} 
-        title="Escuchar y diagnosticar necesidades" 
+      <CharacteristicItem
+        imgUrl={"https://picsum.photos/id/508/580/580"}
+        title="Escuchar y diagnosticar necesidades"
         text={"Nos tomamos el tiempo de entender tus ideas, objetivos y contexto para brindar soluciones a tu medida"}
       />
-      <CharacteristicItem  
-        imgUrl={"https://picsum.photos/id/534/580/580"} 
-        title="Diseñar con creatividad y técnica" 
+      <CharacteristicItem
+        imgUrl={"https://picsum.photos/id/534/580/580"}
+        title="Diseñar con creatividad y técnica"
         text={"Combinamos visión arquitectónica con innovación, funcionalidad y la mejor visualización de cada propuesta"}
         flip
       />
-      <CharacteristicItem  
-        imgUrl={"https://picsum.photos/id/623/580/580"} 
-        title="Planificar y construir con calidad" 
+      <CharacteristicItem
+        imgUrl={"https://picsum.photos/id/623/580/580"}
+        title="Planificar y construir con calidad"
         text={"Optimizamos recursos, mantenemos transparencia y llevamos tu obra a cabo garantizando confianza y excelencia."}
       />
-      <Portfolio/>
-      <Testimonial/>
+      <Portfolio />
+      <Testimonial />
       <Founders />
       <ContactForm />
+      <FabToTop />
     </div>
   )
 }
--- src/components/FabToTop.jsx
+++ src/components/FabToTop.jsx
@@ -2,10 +2,18 @@ import './FabToTop.css'
 
 const FabToTop = () => {
   const handleClick = () => {
-    console.log("Scroll to top pls");
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
   }
-  return <button onClick={ handleClick}>
-    ToTop
+  return <button
+    onClick={handleClick}
+    className="fabToTop__container"
+  >
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" className="fabToTop__label">
+      <path d="M12 8.6L18 14.6L16.6 16L12 11.4L7.4 16L6 14.6L12 8.6Z" fill="#1D1B20" />
+    </svg>
   </button>
 }
 
